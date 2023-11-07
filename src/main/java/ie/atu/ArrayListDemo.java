2d37158f2996b3f54fe8c1ce7edbe493c015f063
Nasir Ahmad <g00419630@atu.ie>
2023-11-07 14:35:51 +0000

    Done

--- src/main/java/ie/atu/ArrayListDemo.java
+++ src/main/java/ie/atu/ArrayListDemo.java
@@ -1,6 +1,33 @@
 package ie.atu;
+import java.util.Scanner;
+
 import java.util.ArrayList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class ArrayListDemo {
+    public static void main(String[] args) {
+
+        int index=100;
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> AnArray = new ArrayList<Integer>(10);
+
+        for (int y=0;y<11;y++)
+        {
+            if(y>9)
+            {
+                System.out.println("Please enter a new number");
+                index=scanner.nextInt();
+            }
+            AnArray.add(index);
+            System.out.println("Element at index "+y+" is "+AnArray.get(y));
+            index+=100;
+        }
 
 
-}
+
+
+
+        }
+    }
