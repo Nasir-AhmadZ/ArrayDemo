665c5255e07d18343e226c89e18a2bd2b761ba45
Nasir Ahmad <g00419630@atu.ie>
2023-11-07 13:39:42 +0000

    Completed step 1 and 2

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,16 +4,19 @@ package ie.atu;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[] ArrayDemo;
+        int x,index=100;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        ArrayDemo= new int[10];
+        for (x=0;x<10;x++)
+        {
+            ArrayDemo[x]=index;
+            index+=100;
+            System.out.println("Element at index "+x+" is "+ArrayDemo[x]);
+        }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+
+
         }
     }
-}
